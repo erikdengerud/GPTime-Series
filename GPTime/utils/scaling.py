2e36698eac8b7d2f2c0f8df48d62a039a1d9157b
erikdengerud <eriko1306@gmail.com>
2020-10-14 14:45:46 +0200

    MASE scaling

---
+++ GPTime/utils/scaling.py
@@ -0,0 +1,35 @@
+
+
+import logging
+import numpy as np
+from typing import Tuple
+import sys
+
+sys.path.append("")
+
+from GPTime.config import cfg
+
+logger = logging.getLogger(__name__)
+
+
+def MASEscale(ts:np.array, freq:str) -> Tuple[np.array, float]:
+        """ Scale time series using scaling from MASE """
+        d = {
+            "Y" : "yearly",
+            "Q" : "quarterly",
+            "W" : "weekly",
+            "D" : "daily",
+            "H" : "hourly",
+            "O" : "other"
+            }
+        period = cfg.scoring.m4.periods[d[freq]]
+        logger.info(period)
+        scale = np.mean(np.abs((ts - np.roll(ts, shift=period))[period:]))
+        ts_scaled = ts / scale
+        return ts_scaled, scale
+
+if __name__ == "__main__":
+    x = np.arange(20, step=2)
+    logger.info(len(x))
+    logger.info(x)
+    logger.info(MASEscale(x, "Q"))
