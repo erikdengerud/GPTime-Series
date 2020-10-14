b25235b905714091961983a70e9a8d7f8571d614
erikdengerud <eriko1306@gmail.com>
2020-10-14 19:10:04 +0200

    sampling in dataloader

--- GPTime/model/data.py
+++ GPTime/model/data.py
@@ -6,12 +6,13 @@ import glob
 import json
 import os
 import numpy as np
-
+from typing import List, Tuple
 import sys
 
 sys.path.append("")
 
 from GPTime.config import cfg
+from GPTime.utils.scaling import MASEscale
 
 logger = logging.getLogger(__name__)
 
@@ -19,60 +20,124 @@ class TSDataset(Dataset):
     """
     Time series dataset.
     TODO:
-        * Horizontal batching for TCN.
+    * Cutoff date.
+    * Sample ts function.
     """
-    def __init__(self, memory:int, dataset_path:str, convolutions:bool=False):
+    def __init__(
+        self, 
+        memory:int, 
+        dataset_paths:List[str], 
+        frequencies:List[str],
+        cutoff_date:str=None,  
+        convolutions:bool=False,
+        num_samples_per_ts:int=1,
+        horizontal_batch_size:int=1,
+        ) -> None:
         super(TSDataset, self).__init__()
         self.memory = memory
         self.convolutions = convolutions
-        # read data into memory
-        ts = []
-        dir_names = []
-        paths = glob.glob(dataset_path + "*") if dataset_path[-1] == "/" else glob.glob(dataset_path + "/*")
-        if len(paths) == 0:
-            logger.warning("No directories in dataset path.")
-        for path in paths:
-            dir_names.append(path)
-            if not os.path.isdir(path):
-                logger.warning("Not all paths in dataset folder are folders.")
-        logger.debug(f"Number of directories: {len(dir_names)}")
+        self.num_samples_per_ts = num_samples_per_ts
+        self.horizontal_batch_size = horizontal_batch_size
 
-        for dir_name in dir_names[0:1]:
-            fnames = glob.glob(path + "/*")
-            for fname in fnames:
-                if not os.path.isfile(fname):
-                    logger.warning("File path is not a file.")
-                with open(fname, "r") as f:
-                    in_file = json.load(f)
-                    ts.append(in_file)
-                    f.close()
-            #logger.debug(f"Number of files in directory {dir_name} : {len(fnames)}")
-            logger.debug(f"Number of tim series in dataset: {len(ts)}")
-        self.ts = ts
+        # Read data into memory
+        all_ts = []
+        for dataset_path in dataset_paths:
+            dirs = glob.glob(os.path.join(dataset_path, "*"))
+            for d in dirs:
+                logger.info(f"Loading dir: {d}")
+                fnames = glob.glob(os.path.join(d, "*"))
+                for fname in fnames:
+                    with open(fname, "r") as fp:
+                        json_list = json.load(fp)
+                        fp.close()
+                    for ts in json_list:
+                        if cutoff_date is not None:
+                            # TODO: Date fixing
+                            pass
+                        if ts["frequency"] in frequencies:
+                            new_ts = {
+                                "frequency": ts["frequency"],
+                                "values" : np.array([float(obs["value"]) for obs in ts["observations"]])
+                            }
+                            all_ts.append(new_ts)
+        self.all_ts = all_ts
+        logger.info(f"Total of {len(all_ts)} time series in memory.")
 
     def __len__(self):
-        return len(self.ts)
+        return len(self.all_ts)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> Tuple:
         ids = list(idx)
-        ids_ts =  [self.ts[i] for i in ids]
+        ids_ts = [self.all_ts[i] for i in ids]
         frequencies = [obs["frequency"] for obs in ids_ts]
         values = [obs["values"] for obs in ids_ts]
-        
-        # call sample ts here
+        logger.info(values)
+        for val in values:
+            logger.info(val.dtype)
 
-        values_tensors = torch.from_numpy(np.array(values))
+        # Sample ts
+        samples, labels = self.sample_ts(values, frequencies)
+
+        samples_tensor = torch.from_numpy(samples)
+        labels_tensor = torch.from_numpy(labels)
+
         if self.convolutions:
-            values_tensors = values_tensors.unsqueeze(1)
-        return values_tensors
+            samples_tensor = samples_tensor.unsqueeze(1)
 
-    @staticmethod
-    def sample_ts(ts, frequency):
-        # sample, pad
-        pass
+        return samples_tensor, labels_tensor, frequencies
+
+
+    def sample_ts(self, ts_list:List[np.array], frequency_list:List[str]) -> Tuple[np.array, np.array]:
+        """
+        Create a sample from a time series.
+
+        Sampling algorithm:
+            1. Sample length in range min_length to length of model memory. Frequency dependent.
+            2. Sample end index in range 0 length of series - 1.
+            3. Create the sample by choosing the observations in range [max(0, end_index - length), end_index]
+            4. Use the end_index + 1 entry of the time series as label.
+            5. Scale the sample using the scaling in MASE.
+            6. Scale label using the same scale as for the sample. 
+        """
+        samples = []
+        labels = []
+        for ts, freq in zip(ts_list, frequency_list):
+            min_length = cfg.dataset.dataset_params.sampling.min_length[freq]
+            max_length = self.memory + self.horizontal_batch_size
+
+            length = np.random.randint(min_length, max_length)
+            end_index = np.random.randint(min_length, len(ts)-1)
+            logger.info(f"length:{length}")
+            logger.info(f"end_index:{end_index}")
+
+            sample = ts[max(0, end_index - length): end_index]
+            label = ts[end_index]
+
+            scale = MASEscale(sample, freq)
+            sample_scaled = sample / scale
+            label_scaled = label / scale
+
+            sample_scaled_pad = np.pad(sample_scaled, (self.memory - len(sample_scaled), 0), mode="constant", constant_values=0)
+
+            samples.append(sample_scaled_pad)
+            labels.append(label_scaled)
+
+        samples = np.vstack(samples)
+        labels = np.vstack(labels)
+
+        return samples, labels
+
 
 if __name__=="__main__":
-    ds = TSDataset(cfg.train.model.memory, cfg.preprocess.path, convolutions=True)
+    ds = TSDataset(
+        memory=cfg.train.model_params_ar.in_features,
+        dataset_paths=cfg.dataset.dataset_params.path.values(),
+        frequencies=[k for k, v in cfg.dataset.dataset_params.frequencies.items() if v],
+        cutoff_date=None,  
+        convolutions=False,
+        num_samples_per_ts=1,
+        horizontal_batch_size=1,)
     logger.debug(ds.__len__())
-    x = ds.__getitem__([1,3,100])
+    x, y, f = ds.__getitem__([1,3,100])
     logger.debug(x.shape)
+    logger.info(x)
--- GPTime/utils/scaling.py
+++ GPTime/utils/scaling.py
@@ -12,7 +12,7 @@ from GPTime.config import cfg
 logger = logging.getLogger(__name__)
 
 
-def MASEscale(ts:np.array, freq:str) -> Tuple[np.array, float]:
+def MASEscale(ts:np.array, freq:str) -> float:#Tuple[np.array, float]:
         """ Scale time series using scaling from MASE """
         d = {
             "Y" : "yearly",
@@ -25,8 +25,8 @@ def MASEscale(ts:np.array, freq:str) -> Tuple[np.array, float]:
         period = cfg.scoring.m4.periods[d[freq]]
         logger.info(period)
         scale = np.mean(np.abs((ts - np.roll(ts, shift=period))[period:]))
-        ts_scaled = ts / scale
-        return ts_scaled, scale
+        #ts_scaled = ts / scale
+        return scale#ts_scaled, scale
 
 if __name__ == "__main__":
     x = np.arange(20, step=2)
